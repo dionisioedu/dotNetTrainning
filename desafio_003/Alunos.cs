aa0fcaf16317d3189615aa3450d4ae84102a0451
cce_cfernandes <cce_cfernandes@uolinc.com>
2021-05-05 09:27:27 -0300

    Desafio 003

---
+++ desafio_003/Alunos.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafio_003
+{
+	public class Alunos
+	{
+		String Nome;
+		double Nota;
+
+		private static int Quantidade { get; set; }
+		private static readonly List<Alunos> ListaDeAlunos = new List<Alunos>();
+
+		public override string ToString()
+		{
+			return String.Format("{0} - {1}", Nome, Nota);
+		}
+
+		public static void TotalDeAlunos(int quantidade)
+		{
+			Quantidade = quantidade;
+		}
+
+		public static int TotalDeAlunos()
+		{
+			return Quantidade;
+		}
+
+		public static void AdicionaAluno(String nome, double nota)
+		{
+			ListaDeAlunos.Add(new Alunos
+			{
+				Nome = nome,
+				Nota = nota
+			});
+		}
+
+		public static String MaiorNota()
+		{
+			Alunos maiorNota = ListaDeAlunos[0];
+
+			if (ListaDeAlunos.Count > 1)
+			{
+				for (int aluno = 1; aluno < ListaDeAlunos.Count; aluno++)
+				{
+					if (ListaDeAlunos[aluno].Nota > maiorNota.Nota)
+					{
+						maiorNota = ListaDeAlunos[aluno];
+					}
+				}
+			}
+
+			return maiorNota.ToString();
+		}
+	}
+}
---
+++ desafio_003/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace desafio_003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual a quantidade de alunos?");
+            String quantidade = Console.ReadLine();
+
+            Alunos.TotalDeAlunos(Int32.Parse(quantidade));
+
+            for (int aluno = 0; aluno < Alunos.TotalDeAlunos(); aluno++)
+			{
+                Console.WriteLine("Qual o nome do aluno de numero {0} ?", aluno + 1);
+                String nome = Console.ReadLine();
+                Console.WriteLine("Qual a nota do aluno de numero {0} ?", aluno + 1);
+                String nota = Console.ReadLine();
+
+				_ = double.TryParse(nota, out double double_nota);
+
+				Alunos.AdicionaAluno(nome, double_nota);
+			}
+
+            Console.WriteLine("O aluno com a maior nota e {0}", Alunos.MaiorNota());
+        }
+    }
+}
