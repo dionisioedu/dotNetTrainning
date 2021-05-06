b5261923ec8d72aede296ec8495398d639f5f483
cce_cfernandes <cce_cfernandes@uolinc.com>
2021-05-05 23:07:55 -0300

    Desafio 006

---
+++ desafio_006/Aluno.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafio_006
+{
+	public class Aluno
+	{
+		public String Nome { get; set; }
+		public int Idade { get; set; }
+		public double Nota { get; set; }
+	}
+}
---
+++ desafio_006/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace desafio_006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Desafio 006\n");
+
+            List<Aluno> alunos = new List<Aluno>();
+
+            alunos.Add(new Aluno { Nome = "Adriana", Idade = 10, Nota = 8 });
+            alunos.Add(new Aluno { Nome = "Beatriz", Idade = 10, Nota = 7 });
+            alunos.Add(new Aluno { Nome = "Carlos", Idade = 10, Nota = 9 });
+
+            var export = new Jitbit.Utils.CsvExport();
+
+            foreach (var aluno in alunos)
+            {
+                export.AddRow();
+                export["Nome"] = aluno.Nome;
+                export["Idade"] = aluno.Idade;
+                export["Nota"] = aluno.Nota;
+            }
+
+            export.ExportToFile("alunos.csv");
+        }
+    }
+}
